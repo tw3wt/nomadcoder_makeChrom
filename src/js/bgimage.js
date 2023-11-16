8ae136e7797eb4cc2a8a9f90e5cb9ee14ce26f04
tw3wt <taewoo136@gmail.com>
2023-11-16 16:31:54 +0900

    forth commit

--- src/js/bgimage.js
+++ src/js/bgimage.js
@@ -4,6 +4,8 @@ const body = document.querySelector("body");
 body.classList.add("img");
 
 const randomBgImg = images[0];
+randomBgImg.style.width = '50%';
+randomBgImg.style.height = 'auto';
 
 const definedBgImg = new Image();
 
