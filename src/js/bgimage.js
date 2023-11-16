2b25c6ed1b5f228fc10689b241eb466db84cf639
tw3wt <taewoo136@gmail.com>
2023-11-16 16:40:58 +0900

    1

--- src/js/bgimage.js
+++ src/js/bgimage.js
@@ -4,8 +4,6 @@ const body = document.querySelector("body");
 body.classList.add("img");
 
 const randomBgImg = images[0];
-randomBgImg.style.width = '15px';
-randomBgImg.style.height = '15px';
 
 const definedBgImg = new Image();
 definedBgImg.src = `https://tw3wt.github.io/nomadcoder_makeChrome/img/${randomBgImg}`;
