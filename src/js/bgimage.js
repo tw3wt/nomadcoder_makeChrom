1d16c5b412a29c2f1a06fefd01dcb0f0413a337c
tw3wt <taewoo136@gmail.com>
2023-11-16 16:41:27 +0900

    1

--- src/js/bgimage.js
+++ src/js/bgimage.js
@@ -7,7 +7,5 @@ const randomBgImg = images[0];
 
 const definedBgImg = new Image();
 definedBgImg.src = `https://tw3wt.github.io/nomadcoder_makeChrome/img/${randomBgImg}`;
-definedBgImg.style.width = '15px';
-definedBgImg.style.height = '15px';
 
 document.body.style.backgroundImage = `url(${definedBgImg.src})`;
